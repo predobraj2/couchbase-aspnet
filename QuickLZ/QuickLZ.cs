95a5562efc0ae28c83fc7bc225fddf01e71985ba
Ruslan Konviser <2001938+predobraj2@users.noreply.github.com>
2013-02-04 11:30:56 +0200

    Update packages & Licenses

--- QuickLZ/QuickLZ.cs
+++ QuickLZ/QuickLZ.cs
@@ -1,129 +1,133 @@
-﻿// DLL wrapper class for the QuickLZ 1.5.x DLL files
-
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
 
 namespace QuickLZ
 {
-	public class QuickLZWrapper
-	{
-		// The C library passes many integers through the C type size_t which is 32 or 64 bits on 32 or 64 bit 
-		// systems respectively. The C# type IntPtr has the same property but because IntPtr doesn't allow 
-		// arithmetic we cast to and from int on each reference. To pass constants use (IntPrt)1234.
-		[DllImport("QuickLZC/quicklz150_64_1.dll")]
-		private static extern IntPtr qlz_compress(byte[] source, byte[] destination, IntPtr size, byte[] scratch);
-		[DllImport("QuickLZC/quicklz150_64_1.dll")]
-		private static extern IntPtr qlz_decompress(byte[] source, byte[] destination, byte[] scratch);
-		[DllImport("QuickLZC/quicklz150_64_1.dll")]
-		private static extern IntPtr qlz_size_compressed(byte[] source);
-		[DllImport("QuickLZC/quicklz150_64_1.dll")]
-		private static extern IntPtr qlz_size_decompressed(byte[] source);
-		[DllImport("QuickLZC/quicklz150_64_1.dll")]
-		private static extern int qlz_get_setting(int setting);
+    /// <summary>
+    /// C# DLL wrapper class for the QuickLZ 1.5.x DLL files
+    /// For QuickLZ documentation please check http://www.quicklz.com
+    /// Please note that QuickLZ enterprise license is not FREE for more companies with more than 1 employees, so make sure you purchase licenses at http://www.quicklz.com/order.html
+    /// Note: QuickLZ Dlls should be present in QuickLZC folder and copied to the same folder inside the bin folder of your app.
+    /// </summary>
+    public class QuickLZWrapper
+    {
+        // The C library passes many integers through the C type size_t which is 32 or 64 bits on 32 or 64 bit 
+        // systems respectively. The C# type IntPtr has the same property but because IntPtr doesn't allow 
+        // arithmetic we cast to and from int on each reference. To pass constants use (IntPrt)1234.
+        [DllImport("QuickLZC/quicklz150_64_1.dll")]
+        private static extern IntPtr qlz_compress(byte[] source, byte[] destination, IntPtr size, byte[] scratch);
+        [DllImport("QuickLZC/quicklz150_64_1.dll")]
+        private static extern IntPtr qlz_decompress(byte[] source, byte[] destination, byte[] scratch);
+        [DllImport("QuickLZC/quicklz150_64_1.dll")]
+        private static extern IntPtr qlz_size_compressed(byte[] source);
+        [DllImport("QuickLZC/quicklz150_64_1.dll")]
+        private static extern IntPtr qlz_size_decompressed(byte[] source);
+        [DllImport("QuickLZC/quicklz150_64_1.dll")]
+        private static extern int qlz_get_setting(int setting);
 
-		private readonly byte[] _stateCompress;
-		private readonly byte[] _stateDecompress;
+        private readonly byte[] _stateCompress;
+        private readonly byte[] _stateDecompress;
 
-		public QuickLZWrapper()
-		{
-			_stateCompress = new byte[qlz_get_setting(1)];
-			_stateDecompress = QLZ_STREAMING_BUFFER == 0 ? _stateCompress : new byte[qlz_get_setting(2)];
-		}
+        public QuickLZWrapper()
+        {
+            _stateCompress = new byte[qlz_get_setting(1)];
+            _stateDecompress = QLZ_STREAMING_BUFFER == 0 ? _stateCompress : new byte[qlz_get_setting(2)];
+        }
 
-		public byte[] Compress(byte[] source)
-		{
-			var d = new byte[source.Length + 400];			
-			var s = (uint)qlz_compress(source, d, (IntPtr)source.Length, _stateCompress);
-			var d2 = new byte[s];
-			Array.Copy(d, d2, s);
-			return d2;
-		}
+        public byte[] Compress(byte[] source)
+        {
+            var d = new byte[source.Length + 400];			
+            var s = (uint)qlz_compress(source, d, (IntPtr)source.Length, _stateCompress);
+            var d2 = new byte[s];
+            Array.Copy(d, d2, s);
+            return d2;
+        }
 
-		public byte[] Decompress(byte[] source)
-		{
-			var d = new byte[(uint)qlz_size_decompressed(source)];		
-			var s = (uint)qlz_decompress(source, d, _stateDecompress);
-			return d;
-		}
+        public byte[] Decompress(byte[] source)
+        {
+            var d = new byte[(uint)qlz_size_decompressed(source)];		
+            var s = (uint)qlz_decompress(source, d, _stateDecompress);
+            return d;
+        }
 
-		public uint SizeCompressed(byte[] source)
-		{
-			return (uint)qlz_size_compressed(source);
-		}
+        public uint SizeCompressed(byte[] source)
+        {
+            return (uint)qlz_size_compressed(source);
+        }
 
-		public uint SizeDecompressed(byte[] source)
-		{
-			return (uint)qlz_size_decompressed(source);
-		}
+        public uint SizeDecompressed(byte[] source)
+        {
+            return (uint)qlz_size_decompressed(source);
+        }
 
-		public uint QLZ_COMPRESSION_LEVEL
-		{
-			get
-			{
-				return (uint)qlz_get_setting(0);
-			}
-		}
+        public uint QLZ_COMPRESSION_LEVEL
+        {
+            get
+            {
+                return (uint)qlz_get_setting(0);
+            }
+        }
 
-		public uint QLZ_SCRATCH_COMPRESS
-		{
-			get
-			{
-				return (uint)qlz_get_setting(1);
-			}
-		}
+        public uint QLZ_SCRATCH_COMPRESS
+        {
+            get
+            {
+                return (uint)qlz_get_setting(1);
+            }
+        }
 
-		public uint QLZ_SCRATCH_DECOMPRESS
-		{
-			get
-			{
-				return (uint)qlz_get_setting(2);
-			}
-		}
+        public uint QLZ_SCRATCH_DECOMPRESS
+        {
+            get
+            {
+                return (uint)qlz_get_setting(2);
+            }
+        }
 
-		public uint QLZ_VERSION_MAJOR
-		{
-			get
-			{
-				return (uint)qlz_get_setting(7);
-			}
-		}
+        public uint QLZ_VERSION_MAJOR
+        {
+            get
+            {
+                return (uint)qlz_get_setting(7);
+            }
+        }
 
-		public uint QLZ_VERSION_MINOR
-		{
-			get
-			{
-				return (uint)qlz_get_setting(8);
-			}
-		}
-
-
-		public int QLZ_VERSION_REVISION
-		{
-			// negative means beta
-			get
-			{
-				return qlz_get_setting(9);
-			}
-		}
-
-		public uint QLZ_STREAMING_BUFFER
-		{
-			get
-			{
-				return (uint)qlz_get_setting(3);
-			}
-		}
+        public uint QLZ_VERSION_MINOR
+        {
+            get
+            {
+                return (uint)qlz_get_setting(8);
+            }
+        }
 
 
-		public bool QLZ_MEMORY_SAFE
-		{
-			get
-			{
-				return qlz_get_setting(6) == 1;
-			}
-		}
+        public int QLZ_VERSION_REVISION
+        {
+            // negative means beta
+            get
+            {
+                return qlz_get_setting(9);
+            }
+        }
+
+        public uint QLZ_STREAMING_BUFFER
+        {
+            get
+            {
+                return (uint)qlz_get_setting(3);
+            }
+        }
+
+
+        public bool QLZ_MEMORY_SAFE
+        {
+            get
+            {
+                return qlz_get_setting(6) == 1;
+            }
+        }
 
 
 
-	}
+    }
 }
--- README.md
+++ README.md
@@ -13,7 +13,8 @@ ASP.NET SessionState Provider
 * LZ4 fast compression algorithm supported in version 1.3.1 and was default algorithm if compression enabled for 1.3.1 version.
 * QuickLZ very fast compression algorithm supported in version 1.3.2 and now is default algorithm if compression enabled (in version 1.3.2).
 * LZ4Sharp library used for fastest compression (https://github.com/stangelandcl/LZ4Sharp) and added as project to solution (licensed under BSD License)
-* QuickLZ DLL used from http://www.quicklz.com/download.html
+* QuickLZ DLL used from http://www.quicklz.com/download.html. Please make sure you read license details for QuickLZ at http://www.quicklz.com/order.html
+and purchase commercial license if required.
 * Provider write his configuration during start to Windows Event Log (version 1.3.1) and in addition to log file (starting from version 1.3.2).
   Check Application/CouchbaseSessionStateProvider source (using Event Viewer) and Logs/CouchbaseSessionStateProvider.log file
 * Starting from version 1.3.0, trivial MVC example was removed from solution.
@@ -23,7 +24,8 @@ ASP.NET SessionState Provider
 * You'll need .NET Framework 4 or later to use the precompiled binaries.
 * To build the client, you'll need Visual Studio 2012.
 * The Nuget package for CouchbaseNetClient (http://nuget.org/packages/CouchbaseNetClient) is referenced by Couchbase.AspNet
-* Couchbase Server 1.8
+* Couchbase Server 1.8 or 2.0
+* If you enable QuickLZ compression, please make sure you have QuickLZ dlls in the 'QuickLZC' subfolder inside bin folder of your application.
 
 ## Configuring the SessionState provider
 
@@ -34,26 +36,26 @@ Update the sessionState section in Web.config as follows:
         <add name="Couchbase" type="Couchbase.AspNet.SessionState.CouchbaseSessionStateProvider, Couchbase.AspNet" />
       </providers>
     </sessionState>
-		
+        
 Configure the Couchbase Client as you normally would:
 
     <section name="couchbase" type="Couchbase.Configuration.CouchbaseClientSection, Couchbase"/>	
 
-	<couchbase>
-		<servers bucket="default" bucketPassword="">
-		<add uri="http://127.0.0.1:8091/pools"/>      
-		</servers>
-	</couchbase>
+    <couchbase>
+        <servers bucket="default" bucketPassword="">
+        <add uri="http://127.0.0.1:8091/pools"/>      
+        </servers>
+    </couchbase>
 
 If you would like to use a custom configuration section, you may do so by specifying a value for the "section" attribute of the provider entry (see below).
 
     <section name="couchbaseSession" type="Couchbase.Configuration.CouchbaseClientSection, Couchbase"/>    
 
-	<couchbaseSession>
-		<servers bucket="sessionState" bucketPassword="">
-		<add uri="http://127.0.0.1:8091/pools"/>      
-		</servers>
-	</couchbaseSession>
+    <couchbaseSession>
+        <servers bucket="sessionState" bucketPassword="">
+        <add uri="http://127.0.0.1:8091/pools"/>      
+        </servers>
+    </couchbaseSession>
 
     <sessionState customProvider="Couchbase" mode="Custom">
       <providers>
@@ -109,53 +111,53 @@ Starting from version 1.3.2 logging (using NLog) can be enabled (logging disable
     </sessionState>
 
 Please make sure you create 'Logs' folder in the root folder of your Web application. Logs will be stored in file 'CouchbaseSessionStateProvider.logs' in that folder.
-	
+    
 
 Note that currently, code-based configuration of the CouchbaseClient is not supported.
 
 In code, simply use the Session object as you normally would.
 
-	Session["Message"] = "Couchbase is awesome!";
+    Session["Message"] = "Couchbase is awesome!";
 
 Be sure to mark any user defined types as Serializable.
 
-	[Serializable]
-	public class SessionUser 
-	{
-		public string Username { get; set; }
+    [Serializable]
+    public class SessionUser 
+    {
+        public string Username { get; set; }
 
-		public string Email { get; set; }
-	}
+        public string Email { get; set; }
+    }
 
 ## Configuring the OutputCache provider
 
 Update the outputCache section in Web.config as follows:
 
     <outputCache defaultProvider="CouchbaseCache">
-		<providers>
-			<add name="CouchbaseCache" type="Couchbase.AspNet.OutputCache.CouchbaseOutputCacheProvider, Couchbase.AspNet" section="couchbase-caching"/>
-		</providers>
+        <providers>
+            <add name="CouchbaseCache" type="Couchbase.AspNet.OutputCache.CouchbaseOutputCacheProvider, Couchbase.AspNet" section="couchbase-caching"/>
+        </providers>
     </outputCache>
 
 Configure the Couchbase Client as you normally would:
 
     <section name="couchbase" type="Couchbase.Configuration.CouchbaseClientSection, Couchbase"/>
 
-	<couchbase>
-		<servers bucket="default" bucketPassword="">
-			<add uri="http://127.0.0.1:8091/pools"/>
-		</servers>
-	</couchbase>
+    <couchbase>
+        <servers bucket="default" bucketPassword="">
+            <add uri="http://127.0.0.1:8091/pools"/>
+        </servers>
+    </couchbase>
 
 If you would like to use a custom configuration section, you may do so by specifying a value for the "section" attribute of the provider entry (see below).
 
     <section name="couchbaseSession" type="Couchbase.Configuration.CouchbaseClientSection, Couchbase"/>
 
-	<couchbaseSession>
-		<servers bucket="sessionState" bucketPassword="">
-			<add uri="http://127.0.0.1:8091/pools"/>
-		</servers>
-	</couchbaseSession>
+    <couchbaseSession>
+        <servers bucket="sessionState" bucketPassword="">
+            <add uri="http://127.0.0.1:8091/pools"/>
+        </servers>
+    </couchbaseSession>
 
     <outputCache defaultProvider="CouchbaseCache">
       <providers>
@@ -168,7 +170,7 @@ Once configured, simply enable output cache as you already do with ASP.NET MVC
     [OutputCache(Duration = 60, VaryByParam="foo")]
     public ActionResult Time(string foo)
     {
-    	return Content(DateTime.Now.ToString());
+        return Content(DateTime.Now.ToString());
     }
 
 or with ASP.NET WebForms
